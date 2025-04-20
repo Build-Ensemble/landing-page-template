46fd4511eaad64546eef80b2092bc0b99bd9a0d5
IlyesEssid <ilyes.essid01@gmail.com>
2025-04-20 13:40:26 -0400

    refactor: remove unused DemoAuthProvider from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { DemoAuthProvider } from './context/demo-auth-context';
 import Landing from '../components/layouts/landing/landing';
 const Home = () => {
   return (
-    <DemoAuthProvider>
       <Landing />
-    </DemoAuthProvider>
   );
 };
 
